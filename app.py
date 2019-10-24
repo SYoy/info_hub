2800d0db328beab9fd2b2c10a6b0f72dec07885a
Marius <marius000steger@gmail.com>
2019-10-24 20:20:24 +0200

    weblayout done

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+
+    templateData = {
+        'title': 'Info-Hub Pfiffikus Lernhilfe',
+    }
+    return render_template('index.html', **templateData)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080, passthrough_errors=True)
