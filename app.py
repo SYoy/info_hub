ca97f4acd08423b9686863aca5a06f32d2bd99c3
Marius <marius000steger@gmail.com>
2019-10-25 17:44:57 +0200

    hub ready - calender missing ( on option)

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
-import datetime
+import os
 
 app = Flask(__name__)
 Bootstrap(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    pics = os.listdir('./static/slideshow')
+    DoSlideShow = True if len(pics) > 1 and len(pics) < 10 else False
 
     templateData = {
-        'title': 'Info-Hub Pfiffikus Lernhilfe'#,
-        #'time': datetime.datetime.now().strftime(%H:%M:%S)
+        'title': 'Info-Hub Pfiffikus Lernhilfe',
+        'slideshow': DoSlideShow,
+        'infoMessage': 'Willkommen in der Pfiffikus Lernhilfe Wiesloch!',
+        'pictures': [os.path.join('static','slideshow', pic) for pic in pics]
     }
     return render_template('index.html', **templateData)
 
