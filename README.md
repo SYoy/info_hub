926399e48d844b298b7e0a9aef913374fde932ee
SYoy <45624001+SYoy@users.noreply.github.com>
2019-10-31 11:20:02 +0100

    v 1.0 merge (#3)
    * bugfix creds
    
    * fixed bug with calender event time comparison
    
    * added pipenv

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # info_hub
+
+## Usage - Google Calender:
+- create `config.js` file in `static/js/` and add following content:
+
+```
+var config = {
+    CLIENT_ID : 'enter google client ID from https://console.developers.google.com',
+    API_KEY : 'enter google api key from https://console.developers.google.com',
+    CAL_ID : 'CALENDERID@group.calendar.google.com',
+};
+```
+
+- make the google calend public available and events to it
+
+## Usage - Slideshow:
+
+- add pictures with resolution format 16:9 to `static/slideshow/`-folder and restart the app.py
+
+## Usage - News:
+
+- change the message in `nachricht.json` as follows:
+
+```
+{
+  "inhalt": "write a new message write here!"
+}
+```
+
+- do not change `"inhalt"`
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 import os
+import json
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -9,14 +10,16 @@ Bootstrap(app)
 def index():
     pics = os.listdir('./static/slideshow')
     DoSlideShow = True if len(pics) > 1 and len(pics) < 10 else False
+    with open('nachricht.json') as json_file:
+        message = json.load(json_file)
 
     templateData = {
         'title': 'Info-Hub Pfiffikus Lernhilfe',
         'slideshow': DoSlideShow,
-        'infoMessage': 'Willkommen in der Pfiffikus Lernhilfe Wiesloch!',
+        'infoMessage': message['inhalt'],
         'pictures': [os.path.join('static','slideshow', pic) for pic in pics]
     }
     return render_template('index.html', **templateData)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080, passthrough_errors=True)
+    app.run(debug=True, host='127.0.0.1', port=8080, passthrough_errors=True)
