17b338f9bed9bb30a895a7ab8d6a7d6907867955
Marius <marius000steger@gmail.com>
2019-11-01 14:55:28 +0100

    added shell script

---
+++ start_infohub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export SHELL=/bin/bash
+
+tmux new-sessions -s info -n 'hub' -declare
+
+tmux send-keys -t info:hub.0 'cd ~ && cd info_hub && python3 app.py' ENTER
+
+tmux new-session -s browser -n 'chromium' -declare
+
+sleep 5
+
+tmux send-keys -t browser:chromium.0 'chromium-browser --app=http://127.0.0.1:8080/ --start-fullscreen' ENTER
