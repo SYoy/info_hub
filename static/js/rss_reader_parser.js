00bce8cce6a65213628f15fe3c6289ca4f9d6715
Marius <marius000steger@gmail.com>
2019-11-21 21:38:14 +0100

    random rss event added from swr bw

--- static/js/rss_reader_parser.js
+++ static/js/rss_reader_parser.js
@@ -2,10 +2,11 @@ const swr_bw = 'https://www.swr.de/~rss/swraktuell/swraktuell-bw-100.xml'
 const swr_school = 'http://www.planet-schule.de/rss.xml'
 
 $(document).ready(function() {
-    loadRSS(swr_school, '#RSS-feed');
+    loadRSS(swr_bw, '#RSS-feed');
 });
 
 function loadRSS(link, htmlContainer) {
+    var rand_i = Math.floor(Math.random() * 10);
     var url = link;
     var container = $(htmlContainer);
 
@@ -14,16 +15,47 @@ function loadRSS(link, htmlContainer) {
             console.log(result.error);
         } else {
             var entries = result.feed.entries;
-            for(var i = 0; i < 1; i++){
-                var entry = entries[i];
-                container.append("<li class=\"RSScard\"><p><h2>"
-                    + "<a " +
-                    // "href=\"" + entry.link + "\" target=\"_blank\"
-                    ">" + entry.title + "</a>"
-                    // + "<a>" + entry.content + "</a>"
-                    + "</li>");
-                container.innerHTML=entry.content;
-            }
+            //for(var i = 0; i < 1; i++){
+            //var entry = entries[i];
+
+            var entry = entries[rand_i];
+            container.append("<li>"// <p><h2>"
+                + "<h2 " +
+                // "href=\"" + entry.link + "\" target=\"_blank\"
+                ">" + entry.title + "</h2> \n"
+                + "<div>" + entry.description + "</div>"
+                + "</li>");
+            //}
         }
     });
 }
+
+/*
+var container = document.querySelector('#RSS-feed');
+var num = 1;
+
+feednami.load(swr_school,function(result){
+  if(result.error){
+    console.log(result.error)
+  }
+  else{
+    var entries = result.feed.entries
+    for(var i = 0; i < num; i++){
+      var entry = entries[i]
+      var li = document.createElement('li');
+      var a = document.createElement('h3');
+      var div = document.createElement('div'); //,{ style : 'font-size: 10px;' }
+
+      a.innerHTML = entry.title;
+      // a.href = entry.link;
+
+      div.innerHTML = entry.description;
+
+      li.appendChild(a);
+      li.appendChild(div.firstChild);
+
+      container.appendChild(li);
+    }
+  }
+});
+*/
